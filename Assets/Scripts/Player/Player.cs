7eba17dbdebf776befe20a0830761c3ea18cf6ea
Chumzy <chumzytheseal@gmail.com>
2025-07-27 01:00:32 -0500

    Basic Jumping

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,7 +2,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Walk Settings")]
     public float movementSpeed = 5f;
+
+    [Header("Jump Settings")]
+    public float jumpVelocity = 3;
+    public float jumpTime = 0.4f;
+
+    private float currentJumpTime = 0;
+
+    // Remembers that jump was pressed a moment after
+    public float jumpRememberanceTime = 0.02f;
+    [SerializeField]
+    private float currentJumpRememberanceTime;
+
+    [Header("Ground Detection")]
+
+    public BoxCollider2D groundCollider;
+
+    // Allows player to still count as on ground for a moment after leaving
+    public float coyoteTime = 0.02f;
+    [SerializeField]
+    private float currentCoyoteTime;
+    
+    private bool onGround;
+
     private Rigidbody2D rb;
 
     public void Awake()
@@ -12,8 +36,33 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
-        float movement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        // Vertical Movement
+        onGround = groundCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        currentCoyoteTime = onGround ? coyoteTime : (currentCoyoteTime > 0 ? currentCoyoteTime - Time.deltaTime : 0);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentJumpRememberanceTime = jumpRememberanceTime;
+        }
+
+        if (currentCoyoteTime > 0 && currentJumpRememberanceTime > 0)
+        {
+            currentJumpTime = jumpTime;
+        }
+
+        if (currentJumpTime > 0)
+        {
+            currentJumpTime -= Time.deltaTime;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpVelocity);
+        }
+
+        currentJumpRememberanceTime = currentJumpRememberanceTime > 0 ? currentJumpRememberanceTime - Time.deltaTime : 0;
+
+        // Horizontal Movement
+        float movement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        
         rb.linearVelocity = new Vector2(movement, rb.linearVelocity.y);
+
+
     }
 }
