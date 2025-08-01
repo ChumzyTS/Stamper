0f07ff03b7c4cc788d299c2076c9be3ec208dc75
Aris <solarislamb@gmail.com>
2025-08-01 05:33:44 -0500

    Pause Menu partial functionality
    Esc key opens pause menu and pauses game. quit closes game (but not while in editor). resume unpauses game. Options opens the options menu, but no options menu functionality has been made.

--- Assets/Scripts/Pause Menu.cs
+++ Assets/Scripts/Pause Menu.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField]
+    private KeyCode pauseKey = KeyCode.Escape;
+    public bool paused = false;
+    public GameObject postcard;
+    public GameObject optionsMenu;
+
+    [Header("Buttons")]
+    [SerializeField]
+    public Button resume;
+    public Button mainMenu;
+    public Button options;
+    public Button quit;
+
+
+
     void Start()
     {
         
@@ -11,6 +28,56 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (postcard.activeSelf == false && paused == false)
+            {
+                Pause();
+                
+            } else
+            {
+                Unpause();
+            }
+        }
     }
+
+    void Unpause()
+    {
+        Time.timeScale = 1;
+        postcard.SetActive(false);
+        optionsMenu.SetActive(false);
+        paused = false;
+        options.onClick.RemoveListener(Unpause);
+        mainMenu.onClick.RemoveListener(MainMenu);
+        quit.onClick.RemoveListener(EndGame);
+        options.onClick.RemoveListener(DisplayOptions);
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        postcard.SetActive(true);
+        paused = true;
+        resume.onClick.AddListener(Unpause);
+        mainMenu.onClick.AddListener(MainMenu);
+        quit.onClick.AddListener(EndGame);
+        options.onClick.AddListener(DisplayOptions);
+    }
+
+    void MainMenu()
+    {
+
+    }
+
+    void DisplayOptions ()
+    {
+        postcard.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    void EndGame()
+    {
+        Application.Quit();
+    }
+
 }
