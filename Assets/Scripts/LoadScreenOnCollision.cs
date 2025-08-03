3a623a1615553b512db494e20e9ff21cc28342bc
Chumzy <chumzytheseal@gmail.com>
2025-08-02 21:39:03 -0500

    Door fade out

--- Assets/Scripts/LoadScreenOnCollision.cs
+++ Assets/Scripts/LoadScreenOnCollision.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,12 +6,30 @@ public class LoadScreenOnCollision : MonoBehaviour
 {
     [SerializeField]
     private string sceneName;
+    [SerializeField]
+    private Animator fader;
+    [SerializeField]
+    private float fadeOutTime;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            if (fader)
+            {
+                fader.SetTrigger("FadeOut");
+                StartCoroutine(delay());
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneName);
+            }
         }
+    }
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSecondsRealtime(fadeOutTime);
+        SceneManager.LoadScene(sceneName);
     }
 }
