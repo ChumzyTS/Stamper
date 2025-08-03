8df218b8b202b9208a60dccf072babdb86962073
BrightCerulean <143139426+BrightCerulean@users.noreply.github.com>
2025-08-02 20:56:51 -0500

    Main Menu SFX
    Sound effect on Main Menu Pressing

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,25 +6,31 @@ public class MainMenu : MonoBehaviour
     private Animator animator;
     [SerializeField]
     private Animator boatAnimator;
+    [SerializeField]
+    AudioClip selectSFX;
 
     public void Play()
     {
+        SFXManager.Instance.PlaySFXClip(selectSFX, transform, 1f);
         animator.SetTrigger("Play");
         boatAnimator.SetTrigger("Play");
     }
 
     public void Options()
     {
+        SFXManager.Instance.PlaySFXClip(selectSFX, transform, 1f);
         animator.SetTrigger("Options");
     }
 
     public void Quit()
     {
+        SFXManager.Instance.PlaySFXClip(selectSFX, transform, 1f);
         Application.Quit();
     }
 
     public void Back()
     {
+        SFXManager.Instance.PlaySFXClip(selectSFX, transform, 1f);
         animator.SetTrigger("MainMenu");
     }
 }
